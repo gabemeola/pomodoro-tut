ed9e8a825d9be39e81f8bb364d36a70d538f0879
gabemeola <gabemeola@gmail.com>
2017-04-01 18:13:09 -0600

    added remote-redux-devtools

--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
 // @flow
 import React, { Component } from 'react';
 import { AppContainer } from '!/containers';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
+import devToolsEnhancer from 'remote-redux-devtools';
 import thunk from 'redux-thunk';
 import * as reducers from './redux';
 
 const store = createStore(
-	combineReducers(reducers),
-	applyMiddleware(thunk),
+	combineReducers(
+		reducers
+	),
+	compose(
+		applyMiddleware(thunk),
+		devToolsEnhancer()
+	)
 );
 
 export default class PomodoroTut extends Component {
