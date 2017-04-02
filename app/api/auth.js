4b7cf013542f2f974bbbd82417ded909659b6089
gabemeola <gabemeola@gmail.com>
2017-04-02 11:37:11 -0600

    added auth.js for authentication

---
+++ app/api/auth.js
@@ -0,0 +1,12 @@
+import { firebaseAuth, facebookProvider } from '!/config/constants';
+import { AccessToken } from 'react-native-fbsdk';
+
+export function getAccessToken() {
+	return AccessToken.getCurrentAccessToken();
+}
+
+export function authWithToken() {
+	return firebaseAuth.signInWithCredential(
+		facebookProvider.credential(AccessToken)
+	);
+}
