ac59c2f43b0acee8a9bb5242e93d288e29dff325
gabemeola <gabemeola@gmail.com>
2017-04-02 11:45:25 -0600

    set up reducer to handle authentication

--- app/redux/modules/authentication.js
+++ app/redux/modules/authentication.js
@@ -1,11 +1,53 @@
+const AUTHENTICATING = 'AUTHENTICATING';
+const NOT_AUTHED = 'NOT_AUTHED';
+const IS_AUTHED = 'IS_AUTHED';
+
 const initialState = {
 	isAuthed: false,
 	isAuthenticating: false,
 	authedId: ''
 };
 
+function authenticating() {
+	return {
+		type: AUTHENTICATING,
+	}
+}
+
+function notAuthed() {
+	return {
+		type: NOT_AUTHED,
+	}
+}
+
+function isAuthed(uid) {
+	return {
+		type: IS_AUTHED,
+		uid,
+	}
+}
+
 export default function authentication(state = initialState, action) {
 	switch (action.type) {
+		case AUTHENTICATING:
+			return {
+				...state,
+				isAuthenticating: true,
+			};
+		case NOT_AUTHED:
+			return {
+				...state,
+				isAuthed: false,
+				isAuthenticating: false,
+				authedId: '',
+			};
+		case IS_AUTHED:
+			return {
+				...state,
+				isAuthed: true,
+				isAuthenticating: false,
+				authedId: action.uid,
+			};
 		default:
 			return state;
 	}
