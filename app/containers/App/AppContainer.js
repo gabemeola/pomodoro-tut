62ab270dfb69470b25cdf2c3262848cb0a0a5fb3
gabemeola <gabemeola@gmail.com>
2017-04-09 15:56:41 -0600

    wired up FlashNotification to App Container

--- app/containers/App/AppContainer.js
+++ app/containers/App/AppContainer.js
@@ -2,14 +2,19 @@ import React, { Component, PropTypes } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 import { PomodoroNavigator } from '!/containers';
-import { PreSplash } from '!/components';
+import { PreSplash, FlashNotification } from '!/components';
 import { firebaseAuth } from '!/config/constants';
 import { onAuthChange } from '!/redux/modules/authentication';
+import { hideFlashNotification } from '!/redux/modules/flashNotification';
 
 class AppContainer extends Component {
 	static propTypes = {
-		isAuthenticating: PropTypes.bool.isRequired,
-		isAuthed: PropTypes.bool.isRequired,
+		isAuthenticating: PropTypes.bool,
+		isAuthed: PropTypes.bool,
+		flashNotificationPermanent: PropTypes.bool,
+		flashNotificationLocation: PropTypes.string,
+		flashNotificationText: PropTypes.string,
+		showFlashNotification: PropTypes.bool,
 	}
 	static defaultProps = {
 		isAuthenticating: false,
@@ -20,6 +25,9 @@ class AppContainer extends Component {
 			this.props.dispatch(onAuthChange(user))
 		);
 	}
+	handleHideNotification = () => {
+		this.props.dispatch(hideFlashNotification());
+	}
 	render() {
 		return(
 			<View style={{flex: 1}}>
@@ -27,14 +35,25 @@ class AppContainer extends Component {
 					? <PreSplash/>
 					: <PomodoroNavigator
 							isAuthed={this.props.isAuthed}/>}
+				{this.props.showFlashNotification === true
+					? <FlashNotification
+							text={this.props.flashNotificationText}
+							permanent={this.props.flashNotificationPermanent}
+							location={this.props.flashNotificationLocation}
+							onHideNotification={this.handleHideNotification}/>
+					: null}
 			</View>
 		)
 	}
 }
 
-const mapStateToProps = ({authentication}) => ({
+const mapStateToProps = ({authentication, flashNotification}) => ({
 	isAuthenticating: authentication.isAuthenticating,
 	isAuthed: authentication.isAuthed,
+	flashNotificationPermanent: flashNotification.permanent,
+	flashNotificationLocation: flashNotification.location,
+	flashNotificationText: flashNotification.text,
+	showFlashNotification: flashNotification.showFlashNotification,
 });
 
 export default connect(
