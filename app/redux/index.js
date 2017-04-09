f8a9fb61e136d4764f63b9cadd1d899d3ae47644
gabemeola <gabemeola@gmail.com>
2017-04-09 14:38:57 -0600

    added flashNotification Reducer

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,2 +1,3 @@
 export { default as authentication } from './modules/authentication';
 export { default as activeFooterTab } from './modules/activeFooterTab';
+export { default as flashNotification } from './modules/flashNotification';
