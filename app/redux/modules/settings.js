17e621180d498e824d3d3fa435c6598a42df74c6
gabemeola <gabemeola@gmail.com>
2017-04-09 19:54:44 -0600

    storing settings data to firebase from redux

--- app/redux/modules/settings.js
+++ app/redux/modules/settings.js
@@ -1,3 +1,5 @@
+import { setTimer, setRest } from '!/api/settings';
+
 const ADD_SETTINGS_TIMER_DURATION = 'ADD_SETTINGS_TIMER_DURATION';
 const ADD_SETTINGS_REST_DURATION = 'ADD_SETTINGS_REST_DURATION';
 
@@ -6,17 +8,35 @@ const initialState = {
 	restDuration: 5,
 };
 
-export function addSettingsTimerDuration(duration) {
+function addSettingsTimerDuration(duration) {
 	return {
 		type: ADD_SETTINGS_TIMER_DURATION,
 		duration,
 	}
 }
 
-export function addSettingsRestDuration(duration) {
+function addSettingsRestDuration(duration) {
 	return {
 		type: ADD_SETTINGS_REST_DURATION,
 		duration,
+	}
+}
+
+export function handleAndUpdateTimer(duration) {
+	return function(dispatch, getState) {
+		const { authedId } = getState().authentication;
+
+		return setTimer(duration, authedId)
+			.then(() => dispatch(addSettingsTimerDuration(duration)))
+	}
+}
+
+export function handleAndUpdateRest(duration) {
+	return function(dispatch, getState) {
+		const { authedId } = getState().authentication;
+
+		return setRest(duration, authedId)
+			.then(() => dispatch(addSettingsRestDuration(duration)))
 	}
 }
 
