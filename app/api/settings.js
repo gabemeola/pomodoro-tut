dae987a54bf52829a70f3b840cdea32498b0c02b
gabemeola <gabemeola@gmail.com>
2017-04-09 20:06:37 -0600

    create a function to fetch settings data

--- app/api/settings.js
+++ app/api/settings.js
@@ -9,3 +9,26 @@ export function setRest(duration, uid) {
 	return ref.child(`settings/${uid}/restDuration`)
 		.set(duration)
 }
+
+export function fetchSettings(uid) {
+	return ref.child(`settings/${uid}`)
+		.once('value')
+		.then((snapshot) => {
+			const timerDuration = 20;
+			const restDuration = 5;
+			const settings = snapshot.val();
+
+			// If Settings is NULL then return some default values
+			if(settings === null) {
+				return {
+					timerDuration,
+					restDuration,
+				}
+			}
+
+			return {
+				timerDuration: !!settings.timerDuration ? settings.timerDuration : timerDuration,
+				restDuration: !!settings.restDuration ? settings.restDuration : restDuration,
+			}
+		})
+}
